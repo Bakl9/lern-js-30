adf9df9493ae1a0d66cd690022d9c41d9ce6cb4b
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-17 10:42:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,30 @@ let permissions1 = { canView: true};
 let permissions2 = { canView: true};
 
 Object.assign (user, permissions1, permissions2);
+
+
+
+// let user = {name: "macar"};
+
+// Object.assign(user, {name:'ilysha'});
+
+// alert(user.name)
+
+
+// let user = { name:'ilsya', age:25,};
+
+// let clone = object.assign({}, user)
+
+
+
+let user = { name: ' Ilysha', age: 25,
+    sizes: {
+        height: 182,
+        width: 50,
+    }
+
+};
+
+let clone = Object.assign({}, user);
+
+alert (user.sizes === clone.sizes);
