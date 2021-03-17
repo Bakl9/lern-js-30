61937d9fc97ad2a9904ad4ad5d23a7a1a56b9be4
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-03-17 10:30:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 let user = { name: 'Иван' };
 
 let admin = user;
+\// let description = {
+//     walls: 'while',
+//     table: "brown",
+// }
 
+// let home = {}
+
+// for (let[key] in description) {
+//     description[key] = home[key]
+// };
+
+// home.name = 'ilysha'
+
+// alert(home.name)
